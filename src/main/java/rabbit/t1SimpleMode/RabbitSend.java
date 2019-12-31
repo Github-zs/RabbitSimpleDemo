b13b7836e2b05ac4a7f0e15815d8b5f736e0c51d
Zhai Shuo <38308786+Github-zs@users.noreply.github.com>
2019-12-31 19:06:55 +0800

    Update RabbitSend.java

--- src/main/java/rabbit/t1SimpleMode/RabbitSend.java
+++ src/main/java/rabbit/t1SimpleMode/RabbitSend.java
@@ -1,7 +1,4 @@
-package rabbit.t1SimpleMode;/**
- * Talcloud.com Inc.
- * Copyright (c) 2018-2020 All Rights Reserved.
- */
+package rabbit.t1SimpleMode;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
