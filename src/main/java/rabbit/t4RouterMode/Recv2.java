9e256b1c15fd0f4f4e833b3f1b2ff65bd818af69
Zhai Shuo <38308786+Github-zs@users.noreply.github.com>
2019-12-31 19:11:04 +0800

    Update Recv2.java

--- src/main/java/rabbit/t4RouterMode/Recv2.java
+++ src/main/java/rabbit/t4RouterMode/Recv2.java
@@ -1,7 +1,4 @@
-package rabbit.t4RouterMode;/**
- * Talcloud.com Inc.
- * Copyright (c) 2018-2020 All Rights Reserved.
- */
+package rabbit.t4RouterMode;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
